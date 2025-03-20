d72b976f96625da8c56afb6445913d252dfc589c
Ananya Sahu <ananyasahu59513@gmail.com>
2025-03-20 20:48:20 +0530

    Oops

---
+++ One.java
@@ -0,0 +1,18 @@
+class Two{
+    class Three{
+         void display(int a,String name){
+            int b=a;
+            String name1=name;
+        System.out.println(a+ " " +name);
+    }
+    }
+   
+}
+public class One{
+    public static void main(String args[]){
+        Two obj=new Two();
+        Two.Three obj1=obj.new Three();
+        obj1.display(2,"Ananya");
+
+    }
+}
