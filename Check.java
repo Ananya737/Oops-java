4a7314af9ea53875b100c094d08bd2762f567979
Ananya Sahu <ananyasahu59513@gmail.com>
2025-03-22 09:29:33 +0530

    Check file

---
+++ Check.java
@@ -0,0 +1,8 @@
+public class Check{
+    int x=10;
+    public static void main(String args[]){
+Check obj=new Check();
+obj.x=9;
+System.out.println(obj.x);
+}
+}
--- One.java
+++ One.java
@@ -8,11 +8,16 @@ class Two{
     }
    
 }
+
 public class One{
     public static void main(String args[]){
         Two obj=new Two();
         Two.Three obj1=obj.new Three();
         obj1.display(2,"Ananya");
+        
+     
+            
+
 
     }
 }
