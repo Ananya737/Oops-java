2cf672ad12f5734cc0c4fc0660b7705266ee2edf
Ananya Sahu <ananyasahu59513@gmail.com>
2025-03-22 09:36:04 +0530

    final keyword

--- Check.java
+++ Check.java
@@ -1,8 +1,20 @@
+
+class Override{
+    int x=3;
+}
+
 public class Check{
-    int x=10;
+    //Final variables cannot override values;
+    final int x=10;
     public static void main(String args[]){
 Check obj=new Check();
-obj.x=9;
+obj.x=9; //This will generate an error;
 System.out.println(obj.x);
+
+
+//Variable Overriding.
+Override obj1=new Override();
+obj1.x=9;
+System.out.println(obj1.x);
 }
 }
