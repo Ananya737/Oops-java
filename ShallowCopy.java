8f3afaea992410cd30852d775cbc61117b21c7a2
Ananya Sahu <ananyasahu59513@gmail.com>
2025-03-23 23:17:27 +0530

    update

--- ShallowCopy.java
+++ ShallowCopy.java
@@ -2,17 +2,23 @@ class One{
 int arr[];
 //Constructor
 One(int arr[]){
-this.arr=arr;
+this.arr= new int [arr.length];
+}
+One(One obj1){
+    this.arr=new int [obj1.arr.length];
 }
 void display(){
-    System.out.print(arr);
+    System.out.println(java.util.Arrays.toString(arr));
 }
 }
 
 public class ShallowCopy{
 public static void main(String args[]){
-    int arr[]={10,20,30};
+    int arr[]={9,20,30};
 One obj1=new One(arr);
+One obj2=new One(obj1);
+
 obj1.display();
+obj2.display();
 }
 }
