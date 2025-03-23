cf80d876874d157d10d8e37dcd27927edfe03a4f
Ananya Sahu <ananyasahu59513@gmail.com>
2025-03-23 22:17:18 +0530

    update

--- Constructor.java
+++ Constructor.java
@@ -1,24 +1,45 @@
  class One{
     //Parameterised constructor;
-    int a;
+
+    int arr[];
     String name;
     int age;
-    One(int a){
-        this.a=a;
+    One(int arr[]){
+        this.arr=new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+            this.arr[i]=arr[i];
+        }
     }
+
+    //Constructor overloading;
+
     One(String name,int age){
         this.name=name;
         this.age=age;
     }
     void display(){
-        System.out.println("Name="+ name+","+"Age="+age);
+        System.out.println(a);
+    }
+
+    //Copy Constructor;
+
+    One(One obj){
+        this.a=obj.a;
     }
  }
 
 public class Constructor{
  public static void main(String args[]){
-    One obj=new One("Ananya",22);
+    One obj=new One(9);
+    One obj1=new One(obj);
+
+    // obj.display();
+    // obj1.display();
+
+    obj.a=12;
 
     obj.display();
+    obj1.display();
+    
  }
 }
