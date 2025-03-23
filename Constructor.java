3ced42cf9a6d74ab740f9b1d1e48be8e2c3ebdf0
Ananya Sahu <ananyasahu59513@gmail.com>
2025-03-23 21:27:58 +0530

    Constructor overloading

--- Constructor.java
+++ Constructor.java
@@ -1,17 +1,24 @@
  class One{
     //Parameterised constructor;
     int a;
+    String name;
+    int age;
     One(int a){
         this.a=a;
     }
+    One(String name,int age){
+        this.name=name;
+        this.age=age;
+    }
     void display(){
-        System.out.println(a);
+        System.out.println("Name="+ name+","+"Age="+age);
     }
  }
 
 public class Constructor{
  public static void main(String args[]){
-    One obj=new One(9);
+    One obj=new One("Ananya",22);
+
     obj.display();
  }
 }
